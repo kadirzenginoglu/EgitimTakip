054d1774d97db9b8ed1dad76f2a0e69c869e76c4
KAdir Zenginoglu <kadir@zenginoglu.com>
2024-06-08 20:29:27 +0300

    Company controller repositorye göre düzenlendi

--- EgitimTakip.Web/Controllers/CompanyController.cs
+++ EgitimTakip.Web/Controllers/CompanyController.cs
@@ -1,5 +1,6 @@
 ﻿using EgitimTakip.Data;
 using EgitimTakip.Models;
+using EgitimTakip.Repository.Shared.Abstract;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,13 +11,13 @@ namespace EgitimTakip.Web.Controllers
     [Authorize]
     public class CompanyController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        
-        public CompanyController(ApplicationDbContext context)
+        private readonly IRepository<Company> _repo;
+
+        public CompanyController(IRepository<Company> repo)
         {
-            _context = context;
-            
+            _repo = repo;
         }
+
         public IActionResult Index()
         {
             return View();
@@ -24,46 +25,57 @@ namespace EgitimTakip.Web.Controllers
 
         public IActionResult GetAll()
         {
-            var result = _context.Companies.Where(c=>!c.IsDeleted).Include(c=>c.Employees).ToList();
-            return Json(new { data = result });
+            //var result = _context.Companies.Where(c=>!c.IsDeleted).Include(c=>c.Employees).ToList();
+            return Json(_repo.GetAll());
         }
 
 
         public IActionResult Add(Company company)
         {
 
-            var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            company.AppUserId= Convert.ToInt32(id);
-            _context.Companies.Add(company);
-            _context.SaveChanges();
-            return Ok(company);
+            //var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            //company.AppUserId= Convert.ToInt32(id);
+            //_context.Companies.Add(company);
+            //_context.SaveChanges();
+            //return Ok(company);
+
+            return Ok(_repo.Add(company));
+
         }
         [HttpPost]
         public IActionResult Update(Company company)
         {
-            var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            company.AppUserId = Convert.ToInt32(id);
-            _context.Companies.Update(company);
-            _context.SaveChanges();
-            return Ok(company);
+            //var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            //company.AppUserId = Convert.ToInt32(id);
+            //_context.Companies.Update(company);
+            //_context.SaveChanges();
+            //return Ok(company);
+            return Ok(_repo.Update(company));
+
         }
         [HttpPost]
         public IActionResult Delete(int id)
         {
-            Company company=_context.Companies.Find(id);
-            company.IsDeleted = true;
-            _context.Companies.Update(company);
-            _context.SaveChanges();
-            return Ok(company);
+            //Company company=_context.Companies.Find(id);
+            //company.IsDeleted = true;
+            //_context.Companies.Update(company);
+            //_context.SaveChanges();
+            //return Ok(company);
+            
+            
+            //return Ok(_repo?.Delete(id));
+            //return Ok(_repo.Delete(id) is object);  - Hileli olanı
+            _repo.Delete(id);
+            return Ok();
         }
-        [HttpPost]
-        public IActionResult HardDelete (int id)
-        {
-            Company company = _context.Companies.Find(id);
-            _context.Companies.Remove(company);
-            _context.SaveChanges();
-            return Ok(company);
-        }
+        //[HttpPost]
+        //public IActionResult HardDelete (int id)
+        //{
+        //    Company company = _context.Companies.Find(id);
+        //    _context.Companies.Remove(company);
+        //    _context.SaveChanges();
+        //    return Ok(company);
+        //}
 
     }
 }
