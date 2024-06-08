ab1c7de2fbd959e16c21a56564d9095780ae83b7
KAdir Zenginoglu <kadir@zenginoglu.com>
2024-06-08 18:51:36 +0300

    interface classı tasarlandı

---
+++ Repository/Shared/Abstract/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Repository.Shared.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        ICollection<T> GetAll();
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(int id);
+        T GetById(int id);
+        void Save();
+
+    }
+}
